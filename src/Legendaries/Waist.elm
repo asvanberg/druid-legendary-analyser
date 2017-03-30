d0b663795791fa9adbccc556fa7a60239f7991dc
Andreas Svanberg <andreass@dsv.su.se>
2017-03-30 11:47:40 +0200

    Added support for the random bloom from belt

--- src/Legendaries/Waist.elm
+++ src/Legendaries/Waist.elm
@@ -1,39 +1,94 @@
 module Legendaries.Waist exposing (Model, init, parse, bonusHealing)
 
 import Dict exposing (Dict)
-
+import Time exposing (Time, millisecond)
 import Util.Maybe exposing ((?))
 
 import WarcraftLogs.Models exposing (Event, Event(..))
 
-type Model = Model (Dict Druid BonusHealing)
+type Model = Model (Dict CharacterID Druid)
 
-type alias Druid = Int
+type alias CharacterID = Int
+type alias Druid =
+  { bonusHealing : Int
+  , lookingForDefaultBloom : DefaultBloom
+  }
+type DefaultBloom = Yes Time | No
 type alias BonusHealing = Int
+
+timeForDefaultBloom : Time
+timeForDefaultBloom = 100 * millisecond
 
 init : Model
 init = Model Dict.empty
 
 parse : Event -> Model -> Model
 parse event (Model druids) =
-  case event of
+  Model <| case event of
     EncounterStart _ ->
-      Model Dict.empty
+      Dict.empty
 
-    Heal {sourceID, amount, overheal, ability} ->
+    RefreshBuff event ->
+      let
+        (Model newDruids) = parse (RemoveBuff event) (Model druids)
+      in
+        newDruids
+
+    RemoveBuff {timestamp, sourceID, ability} ->
+      if ability.id == 33763 then
+        let
+          druid =
+            getDruid druids sourceID
+          newDruid =
+            { druid | lookingForDefaultBloom = Yes timestamp }
+        in
+          Dict.insert sourceID newDruid druids
+      else
+        druids
+
+    Heal {timestamp, sourceID, amount, overheal, ability} ->
       if ability.id == 33778 then
         let
-          baseHeal = (amount + overheal) // 3
-          belt = max 0 (amount - baseHeal)
-          currentBonus = Dict.get sourceID druids ? 0
+          druid =
+            getDruid druids sourceID
+          baseHeal =
+            (amount + overheal) // 3
+          bloomIncrease =
+            max 0 (amount - baseHeal)
+          belt =
+            case druid.lookingForDefaultBloom of
+              Yes refreshed ->
+                if timestamp - refreshed <= timeForDefaultBloom then
+                  bloomIncrease
+                else
+                  amount
+
+              No ->
+                amount
+          newDruid =
+            { druid
+            | bonusHealing = druid.bonusHealing + belt
+            , lookingForDefaultBloom = No
+            }
         in
-          Model <| Dict.insert sourceID (belt + currentBonus) druids
+          Dict.insert sourceID newDruid druids
       else
-        Model druids
+        druids
 
     _ ->
-      Model druids
+      druids
 
 bonusHealing : Model -> Int -> Int
 bonusHealing (Model druids) druid =
-  Dict.get druid druids ? 0
+  (Maybe.map .bonusHealing <| Dict.get druid druids) ? 0
+
+getDruid : Dict CharacterID Druid -> CharacterID -> Druid
+getDruid druids characterID =
+  case Dict.get characterID druids of
+    Just druid ->
+      druid
+
+    Nothing ->
+      { bonusHealing = 0
+      , lookingForDefaultBloom = No
+      }
